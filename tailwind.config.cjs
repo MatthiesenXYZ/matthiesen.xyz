3c3112bf01910a3214b928f64f9fd74b1062089c
Adam Matthiesen <amatthiesen@outlook.com>
2023-12-28 05:24:07 -0800

    setup daisyUI and set theme colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,5 +9,11 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  daisyui: {
+    themes: true,
+  },
+  plugins: [
+    require("daisyui"),
+    require("@tailwindcss/typography"),
+  ],
 };
