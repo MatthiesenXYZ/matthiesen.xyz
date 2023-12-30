6a18807948ea91f4ffc35994208ac83847a9b179
Adam Matthiesen <amatthiesen@outlook.com>
2023-12-30 11:53:59 -0800

    more cleanup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,18 +2,9 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  daisyui: {
-    themes: true,
-  },
+  theme: { extend: { fontFamily: { sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans] } } },
+  daisyui: { themes: true },
   plugins: [
     require("daisyui"),
-    require("@tailwindcss/typography"),
-  ],
+    require("@tailwindcss/typography") ],
 };
