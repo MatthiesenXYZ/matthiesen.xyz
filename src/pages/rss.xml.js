ea646c4e156cb8c73896ec3f24d76e8fd56f91d9
Adam Matthiesen <amatthiesen@outlook.com>
2023-12-31 23:11:59 -0800

    rss page and processing updates, and new SVG file for favicon image

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,14 +1,9 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-import sanitizeHtml from 'sanitize-html';
-import MarkdownIt from 'markdown-it';
-const parser = new MarkdownIt();
 
 export async function GET(context) {
-  const blog = await getCollection("blog");
-
+  const blog = await getCollection("blog", ({ data }) => { return !data.draft && data.publishDate < new Date(); });
   blog.sort(function (a, b) { return b.data.publishDate.valueOf() - a.data.publishDate.valueOf(); });
-
   return rss({
     title: "Matthiesen IT Services & Support",
     description: "From helping you setup your computer, to building a datacenter.  From a Support Center to a Managed Service provider. Any IT Project is not to big or to small, Need a website? No Problem!  Looking to reduce monthly or yearly costs and switch to the Open Source alternative? Just Contact us for help!",
