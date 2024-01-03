c8ea6f0b4b22e3eca53fa73d7f9c7b756feb79aa
Adam Matthiesen <amatthiesen@outlook.com>
2024-01-02 23:18:38 -0800

    merge snipcart... hopefully it works now

---
+++ src/components/snipcart/astro.ts
@@ -0,0 +1,6 @@
+export { default as SnipcartSetup } from './src/SnipcartSetup.astro';
+export { default as CustomerSignIn } from './src/CustomerSignIn.astro';
+export { default as CartTotalPrice } from './src/CartTotalPrice.astro';
+export { default as CartItemsCount } from './src/CartItemsCount.astro';
+export { default as CartCheckout } from './src/CartCheckout.astro';
+export { default as Product } from './src/Product.astro';
---
+++ src/components/snipcart/src/main.ts
@@ -0,0 +1,4 @@
+// Write your component's code here!
+
+export * from './types';
+export * from './attributes';
