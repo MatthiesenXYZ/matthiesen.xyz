da63cb5f6da4c873e361d41deb37b55d2e0d059b
Adam Matthiesen <amatthiesen@outlook.com>
2024-01-02 18:48:12 -0800

    update ghost to pull all keys from ENV variables instead of .ts file and updated layout of ghostpost and slug

--- src/lib/ghost.ts
+++ src/lib/ghost.ts
@@ -1,9 +1,9 @@
 import GhostContentAPI from '@tryghost/content-api';
 
 export const ghostClient = new GhostContentAPI({
-    url: 'https://ghost.matthiesen.xyz',
+    url: import.meta.env.CONTENT_API_URL,
     key: import.meta.env.CONTENT_API_KEY,
-    version: 'v5.0',
+    version: import.meta.env.CONTENT_API_VER,
 });
 
 const getGhostPosts = async () => {
