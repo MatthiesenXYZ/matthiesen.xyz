e913cd73a137931bac48b83180d42e7f4f19a0cb
Adam Matthiesen <amatthiesen@outlook.com>
2024-01-04 06:53:30 -0800

    update website accessability, add a robots.txt generator.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import solid from '@astrojs/solid-js';
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://matthiesen.xyz",
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [tailwind(), mdx(), sitemap(), solid(), sentry(), spotlightjs(), robotsTxt()],
   redirects: {
     '/rss': '/rss.xml'
-  },
+  }
 });
