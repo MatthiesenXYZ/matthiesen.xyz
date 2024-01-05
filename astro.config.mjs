45fcbc4bde5c2f0dc7ca0cadcdff46ff8c1ed2e5
Adam Matthiesen <amatthiesen@outlook.com>
2024-01-04 19:21:10 -0800

    removed extra entry

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import spotlightjs from "@spotlightjs/astro";
 import robotsTxt from "astro-robots-txt";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
