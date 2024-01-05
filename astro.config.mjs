9297be7e7772f45a124bcbbc1f3e29e74abc14a0
Adam Matthiesen <amatthiesen@outlook.com>
2024-01-04 19:10:43 -0800

    first SSR cloudflare test... please work.  lol

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,15 @@ import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
 import robotsTxt from "astro-robots-txt";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://matthiesen.xyz",
   integrations: [tailwind(), mdx(), sitemap(), solid(), sentry(), spotlightjs(), robotsTxt()],
   redirects: {
     '/rss': '/rss.xml'
-  }
+  },
+  output: "server",
+  adapter: cloudflare()
 });
