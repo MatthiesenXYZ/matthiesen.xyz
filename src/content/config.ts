377223cc80654bd850b754069fb6a76dcea6638e
Adam Matthiesen <amatthiesen@outlook.com>
2024-01-06 17:32:05 -0800

    image optimization

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,24 +1,66 @@
-import { defineCollection } from 'astro:content';
-import { hddReportSchema, servicesSchema, storeSchema, teamSchema, testimonialsSchema } from '../lib/schemas'
+import { defineCollection, z } from 'astro:content';
+import defaultProfileImage from '@assets/default-review.jpg';
 
-const team = defineCollection({ 
-  type: "data", schema: teamSchema 
-});
-
-const hddReport = defineCollection({ 
-  type: "data", schema: hddReportSchema 
+const store = defineCollection({ 
+  type: "data", 
+  schema: ({ image }) => z.object({
+    id: z.string(),
+    name: z.string(),
+    url: z.string(),
+    image: image(),
+    price: z.number(),
+    description: z.string(), 
+  }),
 });
 
 const testimonials = defineCollection({ 
-  type: "data", schema: testimonialsSchema 
+  type: "data", 
+  schema: ({ image }) => z.object({
+    name: z.string(),
+    postDate: z.string(),
+    postSource: z.object({
+      desc: z.string(),
+      link: z.string().optional(),
+    }).optional(),
+    avatar: image().default(defaultProfileImage),
+    title: z.string(),
+    body: z.string(),
+  }),
+});
+
+const team = defineCollection({ 
+  type: "data", 
+  schema: ({ image }) => z.object({
+    name: z.string(),
+    title: z.string(),
+    profileurl: z.string().optional(),
+    avatar: image().default(defaultProfileImage),
+  }),
 });
 
 const services = defineCollection({ 
-  type: "data", schema: servicesSchema 
+  type: "data", 
+  schema: z.object({
+    title: z.string(),
+    icon: z.string().default('bi bi-balloon'),
+    description: z.string(),
+  }),
 });
 
-const store = defineCollection({ 
-  type: "data", schema: storeSchema 
+const hddReport = defineCollection({ 
+  type: "data", 
+  schema: z.object({
+    serial: z.string(),
+    make: z.string(),
+    model: z.string(),
+    capacity: z.string(),
+    size: z.string(),
+    type: z.string(),
+    isSSD: z.boolean(),
+    reportDate: z.coerce.date(),
+    manuDate: z.coerce.date(),
+    pdf: z.string(),
+  }),
 });
 
 export const collections = {
