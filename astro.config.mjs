06b65e30e315b1be2af119be71a344bd1e0d55df
Adam Matthiesen <amatthiesen@outlook.com>
2024-01-09 01:34:41 -0800

    prep for extra layout removal - test sync

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,15 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import netlify from "@astrojs/netlify";
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://matthiesen.xyz",
   integrations: [tailwind({
     applyBaseStyles: false,
-    nesting: true,
-  }), sitemap(), robotsTxt()],
+    nesting: true
+  }), sitemap(), robotsTxt(), vue()],
   redirects: {
     '/rss': '/rss.xml'
   },
