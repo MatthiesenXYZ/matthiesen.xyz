9e64ca0ea0fa577594e0aeb3f204cde048264040
Adam Matthiesen <amatthiesen@outlook.com>
2024-01-09 02:20:16 -0800

    readd vars to rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,12 @@
 import rss from "@astrojs/rss";
 import { getGhostPosts } from "@lib/ghost";
+import { RSSdescription, Title } from "headerVars";
 
 export async function GET() {
   const posts = await getGhostPosts();
   return rss({
-    title: "Matthiesen IT Services & Support",
-    description: "From helping you setup your computer, to building a datacenter.  From a Support Center to a Managed Service provider. Any IT Project is not to big or to small, Need a website? No Problem!  Looking to reduce monthly or yearly costs and switch to the Open Source alternative? Just Contact us for help!",
+    title: Title,
+    description: RSSdescription,
     site: import.meta.env.SITE,
     stylesheet: '/rss/styles.xsl',
     items: posts.map((post) => ({
