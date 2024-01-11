576e63b3bb623a2c20e3ad21401c2a6be4cf10b7
Adam Matthiesen <amatthiesen@outlook.com>
2024-01-10 16:39:58 -0800

    update ghost config.  optimizations

--- src/lib/ghost.ts
+++ src/lib/ghost.ts
@@ -1,31 +1,63 @@
+// IMPORT Ghost Content API & Ghost Types
 import GhostContentAPI from '@tryghost/content-api';
+import type {PostOrPage, PostsOrPages, Authors, Tag, Tags} from './ghostTypes';
+
+// IMPORT ENV Variables
 const env = import.meta.env;
 
-export const PI = ['authors', 'tags']
+// SET GHOST FILTERS AND VERSION
+const curAPIVer:string = 'v5.0';
+const iPost = ['authors', 'tags'];
 
-export const ghostClient = new GhostContentAPI({ 
-    url: env.CONTENT_API_URL, key: env.CONTENT_API_KEY, 
-    version: env.CONTENT_API_VER })
+// Create Local Ghost Client
+const ghostClient = new GhostContentAPI({ 
+    key: env.CONTENT_API_KEY, url: env.CONTENT_API_URL, 
+    version: curAPIVer
+})
 
+// Get Posts (General "ALL")
 export const getGhostPosts = async () => {
-    const posts = await ghostClient.posts.browse({
-        limit: 'all', include: `${PI}`, filter: 'visibility:public' }) 
-    return posts;
+    const ghostPosts: PostsOrPages = await ghostClient.posts.browse({
+        limit:'all',include:iPost,filter:'visibility:public' }) 
+    return ghostPosts;
 }
 
-export const getGhostFeaturedPosts = async () => {
-    const posts = await ghostClient.posts.browse({
-        limit: '3', include: `${PI}`, filter: 'featured:true' })
-    return posts;
+// Get Posts (Featured "setlimit")
+export const getGhostFeaturedPosts = async (flimit: string) => {
+    const ghostFeaturedPosts: PostsOrPages = await ghostClient.posts.browse({
+        limit:`${flimit}`,include:iPost,filter:'featured:true' })
+    return ghostFeaturedPosts;
 }
 
-export const getGhostAuthors = async () => {
-    const authors = await ghostClient.authors.browse()
-    return authors;
+// Get Post (By Slug)
+export const getGhostPostbySlug = async (slug: string) => {
+    const ghostPostbySlug: PostOrPage = await ghostClient.posts.read({
+        slug:`${slug}`,include:iPost })
+    return ghostPostbySlug;
 }
 
+// Get Post (By Tag)
+export const getGhostPostsbyTag = async (tagSlug: string) => {
+    const ghostPostsbyTag: PostsOrPages = await ghostClient.posts.browse({
+        filter:`tag:${tagSlug}`,limit:'all',include:iPost })
+    return ghostPostsbyTag;
+}
+
+// Get Tags (General "ALL")
 export const getGhostTags = async () => {
-    const tags = await ghostClient.tags.browse({
-        include: `count.posts` })
-    return tags;
+    const ghostTags: Tags = await ghostClient.tags.browse({ include:`count.posts` })
+    return ghostTags;
+}
+
+// Get Tag (By Slug)
+export const getGhostTagbySlug = async (slug: string) => {
+    const ghostTagbySlug: Tag = await ghostClient.tags.read({
+        slug:`${slug}`,include:`count.posts` })
+    return ghostTagbySlug;
+}
+
+// Get Authors (General "ALL")
+export const getGhostAuthors = async () => {
+    const ghostAuthors: Authors = await ghostClient.authors.browse()
+    return ghostAuthors;
 }
