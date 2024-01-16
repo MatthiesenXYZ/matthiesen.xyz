c8b90fc2fe2ce55db3f48a677cd00154686f8cf8
Adam Matthiesen <amatthiesen@outlook.com>
2024-01-15 22:37:44 -0800

    update names on rss.xml

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,17 +3,17 @@ import { RSSdescription, Title } from "headerVars";
 import { getGhostPosts } from '@adammatthiesen/astro-ghostcms';
 
 export async function GET() {
-  const posts = await getGhostPosts();
+  const ghostPosts = await getGhostPosts();
   return rss({
     title: Title,
     description: RSSdescription,
     site: import.meta.env.SITE,
     stylesheet: '/rss/styles.xsl',
-    items: posts.map((post) => ({
-      title: post.title,
-      pubDate: post.published_at,
-      description: post.excerpt,
-      link: `/blog/${post.slug}/`,
+    items: ghostPosts.map((ghostPost) => ({
+      title: ghostPost.title,
+      pubDate: ghostPost.published_at,
+      description: ghostPost.excerpt,
+      link: `/blog/${ghostPost.slug}/`,
     })),
   });
 }
